478b119e31d5e108f6a9da0a050edcbdb6168425
Habib Ahmad <habibahmad2597@gmail.com>
2024-11-19 16:49:45 +0700

    pindahin navbar

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produk extends Model
 {
-    //
+
+    protected $guarded = ['id'];
 }
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -20,35 +20,7 @@
   </head>
   <body>
     <div class="container-all">
-      <section class="mynavbar">
-        <a href="#" class="logo">CSI</a>
-        <nav>
-          <a href="/">Beranda</a>
-          <a href="/features">Keunggulan</a>
-          <a href="/produk">Produk</a>
-          <a href="/testimoni">Testimoni</a>
-          <a href="/faq">FAQ</a>
-          <a href="/tentang">Tentang</a>
-        </nav>
-        <div class="login">
-          <a href="/login" class="login">Login</a>
-        </div>
-      </section>
-      <!-- Navbar HP -->
-      <!-- <div class="navbar-hp">
-        <a href="#" class="logo">CSI</a>
-        <div class="button"><i class="fa-solid fa-bars"></i></div>
-      </div>
-
-      <div class="navbar-side">
-        <nav>
-          <a href="#">Beranda</a>
-          <a href="#features">Keunggulan</a>
-          <a href="#produk">Produk</a>
-          <a href="#testimoni">Testimoni</a>
-          <a href="#about">Tentang</a>
-        </nav>
-      </div> -->
+      @include('partials.navbar')
       <div class="container-layouts">
         @yield('container')
 
