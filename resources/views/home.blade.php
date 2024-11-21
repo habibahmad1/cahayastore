61c7a6ae3dc4c3a0a49c45831707afae764a2c97
Makayyyy08 <makiyudin08@gmail.com>
2024-11-21 09:32:00 +0700

    ganti image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,21 @@
 @section('container')
 <div class="slider">
     <div class="slides">
-        <div class="slide" style="background-image: url('https://cdn.pixabay.com/photo/2023/10/06/07/14/plant-8297610_1280.jpg');">
+        <div class="slide" style="background-image: url('https://cdn.pixabay.com/photo/2012/03/03/23/54/animal-21668_1280.jpg');">
             <div class="content">
                 <h1>PT. CAHAYA SETIA INDONESIA</h1>
                 <p>"Cahaya Tepat untuk Tangkap yang Hebat, Terangi Laut, Raih Hasil yang Maksimal"</p>
                 <a href="#!" class="btn">Jelajahi Produk</a>
             </div>
         </div>
-        <div class="slide" style="background-image: url('https://cdn.pixabay.com/photo/2022/10/17/09/41/twig-7527181_1280.jpg');">
+        <div class="slide" style="background-image: url('https://cdn.pixabay.com/photo/2014/12/11/02/56/coffee-563797_1280.jpg');">
             <div class="content">
                 <h1>PT. CAHAYA SETIA INDONESIA</h1>
                 <p>"Nikmati Keaslian, Hirup Kenikmatan Bersama Luwak Coffee Authentic"</p>
                 <a href="#!" class="btn">Jelajahi Produk</a>
             </div>
         </div>
-        <div class="slide" style="background-image: url('https://cdn.pixabay.com/photo/2020/02/27/16/38/tree-4885205_1280.jpg');">
+        <div class="slide" style="background-image: url('https://cdn.pixabay.com/photo/2018/08/18/14/26/feet-3614862_1280.jpg');">
             <div class="content">
                 <h1>PT. CAHAYA SETIA INDONESIA</h1>
                 <p>"Sandal Praktis untuk Gaya Dinamis, Setiap Langkah Bersama Kenyamanan"</p>
