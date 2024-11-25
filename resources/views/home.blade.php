af16e1a83c39b2c1306fd2ee1bd4b701754085f5
Habib Ahmad <habibahmad2597@gmail.com>
2024-11-25 15:06:40 +0700

    jelajahi link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,21 +7,21 @@
             <div class="content">
                 <h1>PT. CAHAYA SETIA INDONESIA</h1>
                 <p>"Cahaya Tepat untuk Tangkap yang Hebat, Terangi Laut, Raih Hasil yang Maksimal"</p>
-                <a href="#!" class="btn">Jelajahi Produk</a>
+                <a href="/produk" class="btn">Jelajahi Produk</a>
             </div>
         </div>
         <div class="slide" style="background-image: url('https://cdn.pixabay.com/photo/2014/12/11/02/56/coffee-563797_1280.jpg');">
             <div class="content">
                 <h1>PT. CAHAYA SETIA INDONESIA</h1>
                 <p>"Nikmati Keaslian, Hirup Kenikmatan Bersama Luwak Coffee Authentic"</p>
-                <a href="#!" class="btn">Jelajahi Produk</a>
+                <a href="/produk" class="btn">Jelajahi Produk</a>
             </div>
         </div>
         <div class="slide" style="background-image: url('https://cdn.pixabay.com/photo/2018/08/18/14/26/feet-3614862_1280.jpg');">
             <div class="content">
                 <h1>PT. CAHAYA SETIA INDONESIA</h1>
                 <p>"Sandal Praktis untuk Gaya Dinamis, Setiap Langkah Bersama Kenyamanan"</p>
-                <a href="#!" class="btn">Jelajahi Produk</a>
+                <a href="/produk" class="btn">Jelajahi Produk</a>
             </div>
         </div>
     </div>
