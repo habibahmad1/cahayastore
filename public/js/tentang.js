0cd98d6b37b5dc85e46ae4a1aa21472c66f625f7
Makayyyy08 <makiyudin08@gmail.com>
2024-12-05 13:47:30 +0700

    hidden icon

--- public/js/tentang.js
+++ public/js/tentang.js
@@ -1,39 +1,11 @@
-    // butoon kontak wa
+
     document.getElementById("hubungiButton").addEventListener("click", function () {
         const contactMenu = document.getElementById("contactMenu");
-        contactMenu.classList.toggle("hidden1");
+        contactMenu.classList.toggle("hidden");
     });
 
-    // Bagian Online shop
-    document.querySelectorAll(".online-stores > li > a").forEach(function (dropdownToggle) {
-        dropdownToggle.addEventListener("click", function (event) {
-            event.preventDefault(); // Mencegah aksi default (scroll ke atas)
-
-            const dropdown = this.nextElementSibling; // Ambil elemen dropdown di bawahnya
-
-            // Tutup semua dropdown lainnya
-            document.querySelectorAll(".dropdown").forEach(function (menu) {
-                if (menu !== dropdown) {
-                    menu.classList.add("hidden");
-                }
-            });
-
-            // Toggle visibilitas dropdown yang diklik
-            if (dropdown) {
-                dropdown.classList.toggle("hidden");
-            }
+    document.querySelectorAll(".kontak-links a").forEach(function (link) {
+        link.addEventListener("click", function (event) {
+            event.preventDefault(); // Mencegah scroll ke atas
         });
     });
-
-    // Klik di luar dropdown untuk menutupnya
-    document.addEventListener("click", function (event) {
-        if (!event.target.closest(".online-stores")) {
-            document.querySelectorAll(".dropdown").forEach(function (menu) {
-                menu.classList.add("hidden");
-            });
-        }
-    });
-
-
-
-
