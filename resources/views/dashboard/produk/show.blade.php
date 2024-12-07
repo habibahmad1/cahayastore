2b07403512d87db1ab45109258be4bc3f862730a
Habib Ahmad <habibahmad2597@gmail.com>
2024-12-07 11:32:45 +0700

    edit halaman lihat produk dashboard

--- resources/views/dashboard/produk/show.blade.php
+++ resources/views/dashboard/produk/show.blade.php
@@ -1,7 +1,7 @@
 @extends('dashboard.layouts.main')
 @section('container')
 <div class="detail-product-card" data-name="LED Fishing Light 1">
-    <div class="detail-card">
+    {{-- <div class="">
         @if ($produk->gambar1)
         <div class="img-detail">
             <img src="{{ asset('storage/' . $produk->gambar1) }}" alt="Img-Produk">
@@ -10,7 +10,38 @@
         <div class="img-detail">
             <img src="{{ asset('img/noimg.png' ) }}" alt="Img-Produk">
         </div>
-    @endif
+    @endif --}}
+    <div class="detail-card">
+        <div class="gambar-produk">
+            {{-- Area Gambar Utama --}}
+            <img id="preview-image" src="{{ asset('storage/' . $produk->gambar1) }}" alt="img-produk" class="img-fluid">
+
+            {{-- Thumbnail Gambar --}}
+            <div class="gambar-produk-detail">
+                {{-- Thumbnail Video (jika ada) --}}
+                @if (!empty($produk->video))
+                    <div class="video-place d-inline">
+                        <img src="{{ asset('storage/' . $produk->gambar1) }}" alt="img-thumbnail" class="thumbnail video-img" data-src="{{ asset('storage/' . $produk->video) }}">
+                        <i class="fa-solid fa-play"></i>
+                    </div>
+                @endif
+                <img src="{{ asset('storage/' . $produk->gambar1) }}" alt="img-produk" class="img-fluid thumbnail" data-src="{{ asset('storage/' . $produk->gambar1) }}">
+                {{-- Thumbnail Gambar Tambahan --}}
+                @if (!empty($produk->gambar2))
+                    <img src="{{ asset('storage/' . $produk->gambar2) }}" alt="img-thumbnail" class="thumbnail" data-src="{{ asset('storage/' . $produk->gambar2) }}">
+                @endif
+                @if (!empty($produk->gambar3))
+                    <img src="{{ asset('storage/' . $produk->gambar3) }}" alt="img-thumbnail" class="thumbnail" data-src="{{ asset('storage/' . $produk->gambar3) }}">
+                @endif
+                @if (!empty($produk->gambar4))
+                    <img src="{{ asset('storage/' . $produk->gambar4) }}" alt="img-thumbnail" class="thumbnail" data-src="{{ asset('storage/' . $produk->gambar4) }}">
+                @endif
+                @if (!empty($produk->gambar5))
+                    <img src="{{ asset('storage/' . $produk->gambar5) }}" alt="img-thumbnail" class="thumbnail" data-src="{{ asset('storage/' . $produk->gambar5) }}">
+                @endif
+            </div>
+        </div>
+    </div>
 
 
         <div class="detail-info">
@@ -29,10 +60,29 @@
             <p><b>Berat</b> : {{ $produk->berat }}Kg</p>
             <p><b>Diskon</b> : {{ $produk->diskon }}%</p>
             <h4><b>Harga</b> :Rp {{ $produk->harga }}</h4>
-            <div class="button">
+            <div class="button d-flex">
+                <div class="me-auto"></div>
                 <a href="/dashboard/produk" class="kembali-button">Kembali</a>
             </div>
+
         </div>
     </div>
 </div>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        // Ambil elemen thumbnail
+        const thumbnails = document.querySelectorAll(".thumbnail");
+        const previewImage = document.getElementById("preview-image");
+
+        // Tambahkan event click ke setiap thumbnail
+        thumbnails.forEach(thumbnail => {
+            thumbnail.addEventListener("click", function () {
+                // Perbarui src gambar utama dengan data-src dari thumbnail yang diklik
+                const newSrc = thumbnail.getAttribute("data-src");
+                previewImage.setAttribute("src", newSrc);
+            });
+        });
+    });
+</script>
 @endsection
