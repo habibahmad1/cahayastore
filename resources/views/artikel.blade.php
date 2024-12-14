65c59fd501229283619b4a9bf4df2a3b16b8dc6a
Habib Ahmad <habibahmad2597@gmail.com>
2024-12-14 11:58:02 +0700

    edit artikel

--- resources/views/artikel.blade.php
+++ resources/views/artikel.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('container')
-    <section class="testimoni" id="testimoni">
+    {{-- <section class="testimoni" id="testimoni">
         <div class="text-testimoni">
             <h1>Apa Kata Customer Kami?</h1>
             <div class="next-text">
@@ -172,7 +172,82 @@
             </div>
 
         </div>
-    </section>
+    </section> --}}
 
+    <section class="artikel">
+        <div class="artikelPost">
+            {{-- Content Informasi --}}
+            <div class="content-informasi">
+
+                {{-- Hero IMG --}}
+
+                @if ($artikel->count())
+
+                <div class="card my-5 text-center">
+                    @if ($artikel[0]->image)
+
+                        <div class="text-center gambarTiapPost" style="max-height: 350px; overflow:hidden">
+                            <img src="{{ asset('storage/' . $artikel[0]->image) }}"  alt="imgPost" class="rounded">
+                        </div>
+
+                    @else
+                        <img src="https://picsum.photos/seed/{{ $artikel[0]->kategoripost->nama }}/1200/400" class="card-img-top" alt="{{ $artikel[0]->kategoripost->nama }}">
+                    @endif
+
+                    <div class="card-body">
+                      <h3 class="card-title" style="color: #41a77e"><a href="/artikel/{{ $artikel[0]->slug }}" class="text-decoration-none" style="color: #41a77e">{{ $artikel[0]->judul }}</a></h3>
+
+                      <h5>Penulis : <a href="/authors/{{ $artikel[0]->user->username }}" style="color: #41a77e" class="text-decoration-none">{{ $artikel[0]->user->name }}</a> <a href="/categories/{{ $artikel[0]->kategoripost->slug }}" style="color: #41a77e" class="text-decoration-none badge text-bg-danger">{{ $artikel[0]->kategoripost->nama }}</a> </h5>
+
+                      <p class="card-text">{{ $artikel[0]->excerpt }}</p>
+                      <p class="card-text"><small class="text-info fw-bold">{{ $artikel[0]->created_at->diffForHumans() }}</small></p>
+
+                      <a href="/artikel/{{ $artikel[0]->slug }}" class="selengkapnya my-2 d-inline-block" style="color: black">Baca Selengkapnya</a>
+                    </div>
+                </div>
+
+                <div class="all-card">
+                @foreach ($artikel->skip(1) as $post)
+                    <article class="mb-5 artikel-card">
+                        @if ($post->image)
+                            <div class="text-center gambarTiapPost" style="max-height: 350px; overflow:hidden">
+                                <img src="{{ asset('storage/' . $post->image) }}"  alt="imgPost" class="rounded mb-3">
+                            </div>
+                        @else
+                            <div class="text-center gambarTiapPost">
+                                <img src="https://picsum.photos/seed/{{ $post->kategoripost->nama }}/1200/600"  alt="imgPost" class="rounded mb-3">
+                            </div>
+                            {{-- <div class="text-center gambarTiapPost">
+                                <img src="https://source.unsplash.com/500x400?{{ $post->category->nama }}"  alt="imgPost" class="rounded mb-3">
+                            </div> --}}
+                        @endif
+
+                        <a href="/artikel/{{ $post->slug }}">
+                            <h2 style="color: #41a77e">{{ $post->judul }}</h2>
+                        </a>
+
+                        <small class="fw-bold">Penulis :<a href="/authors/{{ $post->user->username }}" style="color: #41a77e" > {{ $post->user->name }} </a>
+
+                        <div class="badge text-bg-danger"><a href="/categories/{{ $post->kategoripost->slug }}" class="text-white">{{ $post->kategoripost->nama }}</a></div>
+                            <a class="text-info">{{ $post->created_at->diffForHumans() }}</a>
+                        </small>
+
+
+                        <p>{{ $post->excerpt }}</p>
+
+                        <a href="/artikel/{{ $post->slug }}" class="more" style="color: black">Baca Selengkapnya</a>
+                    </article>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+        @else
+            <p class="text-center fs-4  ">Artikel Tidak Ditemukan</p>
+        @endif
+        <div class="pagination justify-content-center">
+            {{ $artikel->links() }}
+        </div>
+    </div>
+    </section>
 
 @endsection
